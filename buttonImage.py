2ad3ed764b19e5b202e2da037b58c3e0c1a4d36a
ankhuve <erik.c.forsberg@gmail.com>
2013-09-30 22:21:24 +0200

    lite prov på GUI

---
+++ buttonImage.py
@@ -0,0 +1,24 @@
+from tkinter import *
+root=Tk()
+knappar = []
+bokstav = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+for i in range(10):
+    li = []
+    for j in range(10):
+        li.append(j)
+    knappar.append(li)
+
+
+for k in range(10):
+    Label(text=bokstav[k]).grid(column=k+1, row=0)
+for r in range(1,11):
+    Label(text=r).grid(column=0, row=r)
+
+for i in range(1,11):
+    for j in range(1,11):
+        b=Button(root)
+        photo=PhotoImage(file="smiley.gif")
+        b.config(image=photo,width="20",height="20")
+        b.grid(column=j, row=i, sticky=W)
+Label(text="Det här är spelplanen!").grid(column=1, columnspan=10)
+root.mainloop()
