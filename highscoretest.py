1debfa728f80eb58d2b1adf28ae4fcb855e58a77
ankhuve <erik.c.forsberg@gmail.com>
2013-11-24 18:50:08 +0100

    kanske fixad highscore?

--- highscoretest.py
+++ highscoretest.py
@@ -1,14 +1,14 @@
-text = open("highscores.txt", "r")
+text = open("nyfil.txt", "r")
 info = text.read()
-hs_dict = {}
-for entry in info.replace("%", "").splitlines():
+hs_list = []
+for entry in info.splitlines():
     entry = entry.split(" | ")
-    hs_dict[entry[0]] = entry[1]
-sorted_hs = sorted(hs_dict.items(), key=lambda a: float(a[0]), reverse=True)
+    hs_list.append((entry[0],entry[1]))
+sorted_hs = sorted(hs_list, key=lambda a: float(a[0]), reverse=True)
 print(sorted_hs)
-##top_ten_pct = []
-##[top_ten_pct.append(float(sorted_hs[i][0])) for i in range(10)]
-##print(top_ten_pct)
+top_ten_pct = []
+[top_ten_pct.append(float(sorted_hs[i][0])) for i in range(10)]
+print(top_ten_pct)
 
 nyfil = open("nyfil.txt", "w+")
 for i in sorted_hs:
