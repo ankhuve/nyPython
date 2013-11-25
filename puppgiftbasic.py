84e16b7f6e669c9096e0d1486ff55e7c931ea07f
ankhuve <erik.c.forsberg@gmail.com>
2013-11-25 15:40:11 +0100

    "Fullt" fungerande
    Mycket kod ändrad och nerkortad samt gjort en ny klass för skeppen för
    att kunna köra träff sänk. Knappar görs nu via Game.buttonMaker().

--- puppgiftbasic.py
+++ puppgiftbasic.py
@@ -4,23 +4,23 @@ import random
 class Game:
     def __init__(self, root):
         self.SHIPS = [5,4,3,3,2]
+        self.ship_list = []
         self.shots = 0
         self.hits = 0
         self.toggle_cheat = False
+        self.cheater = False
         self.placed_ships = []
         root.title("Sänka Skepp")
         self.box_list = self.createGrid()
-        self.randomizeShipPlacement(self.box_list, self.placed_ships)
+        self.randomizeShipPlacement()
 
     def createGrid(self):
-        Grid.rowconfigure(root,0,weight=1)
-        Grid.columnconfigure(root,0,weight=1)
+        Grid.rowconfigure(root, 0, weight=1)
+        Grid.columnconfigure(root, 0, weight=1)
         columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]        
-        for c in range(10):
-            Label(text=columns[c], fg="darkgreen", bg="#95E895").grid(column=c+1, row=0, sticky=N+S+E+W)
         Label(text="", bg="#95E895").grid(column=0, row=0, sticky=N+S+E+W)
-        for r in range(1,11):
-            Label(text=r, fg="darkgreen", bg="#95E895").grid(column=0, row=r, sticky=N+S+E+W)
+        [Label(text=columns[c], fg="darkgreen", bg="#95E895").grid(column=c+1, row=0, sticky=N+S+E+W) for c in range(10)]
+        [Label(text=r, fg="darkgreen", bg="#95E895").grid(column=0, row=r, sticky=N+S+E+W) for r in range(1,11)]
         box_list = []
         for i in range(1,11):
             row = []
@@ -28,135 +28,128 @@ class Game:
                 coords = (i,j)
                 row.append(Box(coords, root, "lightblue"))
             box_list.append(row)
-        Label(text="Klicka på rutorna ovan för att skjuta!", fg="darkgreen", bg="#95E895").grid(row=11, columnspan=11, sticky=E+W+S+N)
-        Label(text="Pricksäkerhet:", fg="darkgreen", bg="#95E895").grid(row=12, column=0, columnspan=5, sticky=E+W+S+N)
-        Label(text="Antal försök:", fg="darkgreen", bg="#95E895").grid(row=12, column=5, columnspan=6, sticky=E+W+S+N)
+        self.info = Label(text="Klicka på rutorna ovan för att skjuta!", fg="darkgreen", bg="#95E895", font=("",12,"bold")).grid(row=11 ,columnspan=11, sticky=E+W+S+N)
+        Label(text="Pricksäkerhet:", fg="darkgreen", bg="#95E895", font=("",10,"bold")).grid(row=12, column=0, columnspan=5, sticky=E+W+S+N)
+        Label(text="Antal skott:", fg="darkgreen", bg="#95E895", font=("",10,"bold")).grid(row=12, column=5, columnspan=6, sticky=E+W+S+N)
+        cheatbutton = self.buttonMaker(root, "fuska lite...", fg="#95E895", bg="#95E895")
+        cheatbutton.configure(relief=FLAT, width="7", height="1", command=self.cheat)
+        cheatbutton.grid(column=9, columnspan=2, row=11, sticky=E+W)
         root.resizable(0,0)
-        self.createCheatButton(self.placed_ships)
         return box_list
             
-    def randomizeShipPlacement(self, box_list, placed_ships):
+    def randomizeShipPlacement(self):
+        self.order = 0
         for ship in self.SHIPS:
             placement_check = False
             while placement_check != True:
-                placement_check, direction, x, y = self.controlPlacement(box_list, ship)
+                placement_check, direction, x, y = self.controlPlacement(ship)
             if direction == "h":
                 for n in range(ship):
-                    box_list[x-1+n][y-1].status = 1
-                    placed_ships.append(box_list[x-1+n][y-1])
+                    self.placeShip(n, 0, x, y)
+                self.ship_list.append(Ship(ship, (x ,y), direction))
             elif direction == "v":
                 for n in range(ship):
-                    box_list[x-1][y-1+n].status = 1
-                    placed_ships.append(box_list[x-1][y-1+n])
-        
-    def controlPlacement(self, box_list, ship):
+                    self.placeShip(0, n, x, y)
+                self.ship_list.append(Ship(ship, (x, y), direction))
+            self.order += 1
+            
+    def controlPlacement(self, ship):
         control = False
         while control == False:
-            start = random.choice(box_list[random.randint(0,9)])
+            start = random.choice(self.box_list[random.randint(0,9)])
             x = start.coords[0]
             y = start.coords[1]
             direction = random.choice(["h", "v"])
             try:
-                for i in range(ship): ## Kolla om det kommer överlappa
+                for i in range(ship):
                     if direction == "h":
-                        if box_list[x-1+i][y-1].status != 0:
-                            control = False
-                            break
-                        else:                       # MÅSTE SNYGGAS TILL FÖR FAN
-                            control = True
+                        control = self.controlBorders(i, 0, x, y)
                     elif direction == "v":
-                        if box_list[x-1][y-1+i].status != 0:
-                            control = False
-                            break
-                        else:
-                            control = True
-                if control == True: ## Kolla runt omkring skeppet
-                    try:
-                        
-                        if direction == "h":
-                            for i in range(ship):
-                                if (
-                                    box_list[x-2+i][y-1].status != 0 # vänster
-                                    or box_list[x-1+i][y-2].status != 0 # ovanför
-                                    or box_list[x+i][y-1].status != 0 # höger       
-                                    or box_list[x-1+i][y].status != 0 # under
-                                    ):
-                                    control = False
-                                    break
-                                else:                                   # MÅSTE SNYGGAS TILL FÖR FAN
-                                    control = True
-                        elif direction == "v":
-                            for i in range(ship):
-                                check = controlBorders(direction)
-                                if (
-                                    box_list[x-2][y-1+i].status != 0 # vänster
-                                    or box_list[x-1][y-2+i].status != 0 # ovanför
-                                    or box_list[x][y-1+i].status != 0 # höger
-                                    or box_list[x-1][y+i].status != 0 # under
-                                    ):
-                                    control = False
-                                    break
-                                else:
-                                    control = True
-                    except IndexError:
-                        return False, direction, x, y
-                return control, direction, x, y
+                        control = self.controlBorders(0, i, x, y)
+                    if control == False:
+                        break
             except IndexError:
                 return False, direction, x, y
-##    def controlBorders(self, direction):   EXPERIMENTELLT
-##        if direction == "h":
-##            check_list = [(x-2+i, y-1),(x-1+i, y-2),(x+i, y-1),(x-1+i, y)]
-##        elif direction == "v":
-##            check_list = [(x-2, y-1+i),(x-1, y-2+i),(x, y-1+i),(x-1, y+i)]
-##
-##        for i in range(ship):
-##            for i in check_list:
-##                if box_list[i[0]][i[1]].status != 0:
-##                    control = False
-##                    break
-##        
-##        return check_list
-        
+            return control, direction, x, y
 
-    def fireInTheHole(self, coords, status, box_list):
-        box = box_list[coords[0]-1][coords[1]-1]
+    def placeShip(self, i, j, x, y):
+        self.box_list[x-1+i][y-1+j].status = 1
+        self.box_list[x-1+i][y-1+j].order = self.order
+        self.placed_ships.append(self.box_list[x-1+i][y-1+j])
+            
+    def controlBorders(self, i, j, x, y):
+        if (
+            self.box_list[x-1+i][y-1+j].status != 0 # kolla överlapp
+            or self.box_list[x-2+i][y-1+j].status != 0 # vänster
+            or self.box_list[x-1+i][y-2+j].status != 0 # ovanför
+            or self.box_list[x+i][y-1+j].status != 0 # höger       
+            or self.box_list[x-1+i][y+j].status != 0 # under
+            ):
+            control = False
+        else:                                   
+            control = True
+        return control
+
+    def fireInTheHole(self, coords, status):
+        box = self.box_list[coords[0]-1][coords[1]-1]
         if box.status == 1:
+            shot = "Träff!"
+            self.shots += 1
+            self.ship_list[box.order].hits += 1
             box.status = 3
             box.changeColor("red")
             self.hits += 1
-            self.shots += 1
+            self.updateStats(self.shots, self.hits, shot)
+            if self.hits==(17):
+                self.endGameCheck()
+            elif self.ship_list[box.order].hits == self.ship_list[box.order].length:
+                #self.hitNSunk()
+                shot = "Träff och sänk!"
+                self.updateStats(self.shots, self.hits, shot)
         elif box.status == 0:
+            shot = "Bom.."
+            self.shots += 1
             box.status = 2
             box.changeColor("white")
-            self.shots += 1
+            self.updateStats(self.shots, self.hits, shot)
         else:
             pass
-        self.updateStats(self.shots, self.hits)
-        if self.hits==(17):
-            self.endGameCheck()
+
+    def hitNSunk(self): # om man vill ha popup för varje träff sänk? För tillfället avaktiverad.
+        self.popup = Toplevel()
+        self.popup.title("Träff & sänk!")
+        self.popup.configure(background="#95E895")
+        text = Label(
+            self.popup, text="Du sänkte skeppet!",
+            bg="lightgreen", fg="darkgreen", font=("", 12)).grid(columnspan=2, sticky=N+S+E+W, ipadx=10, ipady=10)
+        ok = self.buttonMaker(self.popup)
+        ok.configure(command=self.popup.destroy)
+        ok.grid(columnspan=2, row=3, sticky=N+S+E+W, padx=10, pady=10)
+        self.popup.resizable(0,0)
+        self.popup.mainloop()
             
-    def updateStats(self, shots, hits):
+    def updateStats(self, shots, hits, shot):
         try:
             accuracy = (self.hits/self.shots)*100
         except ZeroDivisionError:
             accuracy = 1
-        Label(text=str(accuracy)[:4]+"%", fg="darkgreen", bg="#95E895").grid(row=12, column=4, sticky=E+W+N+S)
-        Label(text=str(self.shots), fg="darkgreen", bg="#95E895").grid(row=12, column=9, sticky=E+W+N+S)
+        Label(text=shot, fg="darkgreen", bg="#95E895", font=("",12,"bold")).grid(row=11, column=2, columnspan=7, sticky=E+W+S+N)
+        Label(text=str(accuracy)[:4]+"%", fg="darkgreen", bg="#95E895").grid(row=12, column=4, sticky=W+N+S)
+        Label(text=str(self.shots), fg="darkgreen", bg="#95E895").grid(row=12, column=9, sticky=W+N+S)
             
     def endGameCheck(self):
         top_ten_pct = self.getTopTen(self.readFile())
-        print(top_ten_pct)
         if (self.hits/self.shots)*100 > top_ten_pct[-1]:
-            self.highScorePopup(self.shots, self.hits)
+            self.highScorePopup(self.shots, self.hits, "Grattis!",
+                                "Grattis, du lyckades ta dig in på high-score-listan!\nVar god ange ditt namn i rutan nedanför.")
         else:
-            print("Du kvalade inte till high-scoren....")
-            self.exitGame()
+            self.highScorePopup(self.shots, self.hits, "Tyvärr..", "Du lyckades tyvärr inte ta dig in på high-score-listan..")
 
     def exitGame(self):
         root.destroy()
 
-    def restartGame(self, high_scores):
-        high_scores.destroy()
+    def restartGame(self, popup):
+        popup.destroy()
         self.__init__(root)
             
     def readFile(self):
@@ -170,35 +163,47 @@ class Game:
             entry = entry.split(" | ")
             hs_list.append((entry[0],entry[1]))
         sorted_hs = sorted(hs_list, key=lambda a: float(a[0]), reverse=True)
-        
         top_ten_pct = []
         [top_ten_pct.append(float(sorted_hs[i][0])) for i in range(10)]
-        return top_ten_pct # Kanske kan returnera sorted_hs istället och ta [10] vid kontrollen?
+        return top_ten_pct
 
-    def highScorePopup(self, shots, hits):
+    def highScorePopup(self, shots, hits, title, info):
         accuracy = (self.hits/self.shots)*100
         self.popup = Toplevel()
-        self.popup.title("Grattis!")
+        self.popup.title(title)
+        self.popup.configure(background="lightgreen")
         text = Label(
-            self.popup, text="Grattis, du lyckades ta dig in på high-score-listan!\nVar god ange ditt namn i rutan nedanför.",
+            self.popup, text=info,
             bg="lightgreen", fg="darkgreen", font=("", 12)).grid(columnspan=2, sticky=N+S+E+W, ipadx=20, ipady=20)
-        name = StringVar()
-        entry = Entry(self.popup, textvariable=name).grid(row=3, sticky=E+W+N+S)
-        
-        ok_button = Button(
-            self.popup, text="OK!", fg="darkgreen", bg="lightgreen",
-            command=lambda accuracy=str(accuracy)[:4]: self.highScoreEntry(accuracy, self.popup, name)
-            )
-        ok_button.grid(column=1, row=3, sticky=E+W)
+        if title == "Grattis!":
+            name = StringVar()
+            entry = Entry(self.popup, textvariable=name).grid(row=3, sticky=E+W+N+S)
+            ok = self.buttonMaker(self.popup)
+            ok.configure(command=lambda accuracy=str(accuracy)[:4]: self.highScoreEntry(accuracy, self.popup, name))
+            ok.grid(column=1, row=3, sticky=E+W)
+        elif title == "Tyvärr..":
+            ok = self.buttonMaker(self.popup, "Avsluta")
+            ok.configure(command=self.exitGame)
+            ok.grid(row=3, column=1, sticky=N+S+E+W, padx=4, pady=4)
+            restart = self.buttonMaker(self.popup, "Spela igen")
+            restart.configure(command=lambda: self.restartGame(self.popup))
+            restart.grid(column=0, row=3, sticky=N+S+E+W, padx=4, pady=4)
         self.popup.resizable(0,0)
         self.popup.mainloop()
+
+    def buttonMaker(self, win, info="OK", fg="darkgreen", bg="#81C981"):
+        return Button(win, text=info, fg=fg, bg=bg)
+        
 
     def highScoreEntry(self, accuracy, popup, name):
         hs_list = []
         for entry in self.info.splitlines():
             entry = entry.split(" | ")
             hs_list.append((entry[0],entry[1]))
-        hs_list.append((accuracy, name.get()))
+        if self.cheater == False:
+            hs_list.append((accuracy, name.get()))
+        else:
+            hs_list.append((accuracy, str(name.get())+" (med fusk!)"))
         sorted_hs = sorted(hs_list, key=lambda a: float(a[0]), reverse=True)
         updated_hs = open("highscores.txt", "w+")
         for i in sorted_hs:
@@ -211,51 +216,41 @@ class Game:
         high_scores = Toplevel()
         high_scores.title("High-score")
         high_scores.configure(background="lightgreen")
-        Label(high_scores, text="Topp tio", font=("Verdana", 16), fg="darkgreen", bg="#81C981").grid(columnspan=2, sticky=N+S+E+W, ipadx=20, ipady=20)
+        Label(high_scores, text="Topp tio", font=("Verdana", 16, "bold"), fg="darkgreen",
+              bg="#81C981").grid(columnspan=2, sticky=N+S+E+W, ipadx=20, ipady=20)
         for i in range(10):
-            Label(
-                high_scores, text=str(i+1)+". "+str(float(sorted_hs[i][0]))+"% | "+str(sorted_hs[i][1]), 
-                bg="#95E895", fg="darkgreen", font=("", 10)).grid(sticky=N+S+W, ipadx=20, columnspan=2)
-        ok = Button(
-            high_scores, text="Avsluta", fg="darkgreen", bg="#81C981",
-            command=self.exitGame
-            ).grid(row=12, column=1, sticky=N+S+E+W, padx=4, pady=4)
-        play_again = Button(
-            high_scores, text="Spela igen", fg="darkgreen", bg="#81C981",
-            command=lambda: self.restartGame(high_scores)
-            )
-        play_again.grid(column=0, row=12, sticky=N+S+E+W, padx=4, pady=4)
-        
-    def createCheatButton(self, placed_ships):
-        self.cheatbutton = Button(
-            root, text="fuska lite..",fg="#95E895", bg="#95E895", relief=FLAT,
-            command=lambda: self.cheat(self.placed_ships, self.toggle_cheat)
-            )
-        self.cheatbutton.config(width="7", height="1")
-        self.cheatbutton.grid(column=9, columnspan=2, row=11)
+            Label(high_scores, text=str(i+1)+". "+str(float(sorted_hs[i][0]))+"% | "+str(sorted_hs[i][1]), 
+                bg="#95E895", fg="darkgreen", font=("", 10, "bold")).grid(sticky=N+S+W, ipadx=20, columnspan=2)
+        ok = self.buttonMaker(high_scores, "Avsluta")
+        ok.configure(command=self.exitGame)
+        ok.grid(row=12, column=1, sticky=N+S+E+W, padx=4, pady=4)
+        restart = self.buttonMaker(high_scores, "Spela igen")
+        restart.configure(command=lambda: self.restartGame(high_scores))
+        restart.grid(column=0, row=12, sticky=N+S+E+W, padx=4, pady=4)
 
-    def cheat(self, placed_ships, toggle_cheat):
+    def cheat(self):
         if self.toggle_cheat == True: # Om fusket är påslaget
-            for obj in placed_ships:
+            for obj in self.placed_ships:
                 if obj.status == 1:
                     obj.changeColor("lightblue") # Dölj skeppen
             self.toggle_cheat = False # Om fusket är avslaget
         else:
-            for obj in placed_ships:
+            for obj in self.placed_ships:
                 if obj.status == 1:
                     obj.changeColor("grey") # Visa skeppen
-            self.toggle_cheat = True
+            self.cheater, self.toggle_cheat = True, True
 
 class Box(Game):
     def __init__(self, coords, root, color):
         self.status = 0
         self.coords = coords
+        self.order = -1 # Rutor utan skepp får order -1
         self.root = root
         self.button = Button(
-            root, bg=color,
+            self.root, bg=color,
             activebackground="blue",
             relief=GROOVE, cursor="target",
-            command=lambda coords=self.coords, status=self.status: game.fireInTheHole(coords, status, game.box_list)
+            command=lambda coords=self.coords, status=self.status: game.fireInTheHole(coords, status)
             )
         self.button.config(width="5",height="2")
         self.button.grid(column=self.coords[0], row=self.coords[1])
@@ -264,5 +259,15 @@ class Box(Game):
     def changeColor(self, color):
         self.button.configure(bg=color)
 
+class Ship():
+    def __init__(self, ship, coords, direction):
+        self.length = ship
+        self.direction = direction
+        self.hits = 0
+        self.coords = coords
+
+
+### Main ###
 root=Tk()
 game = Game(root)
+############
