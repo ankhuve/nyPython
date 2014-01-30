30b6d6c21b0ce2a7cddc430d2966240ed9824acb
ankhuve <erik.c.forsberg@gmail.com>
2014-01-30 11:40:10 +0100

    Fixd

--- Tilda/labb2troll.py
+++ Tilda/labb2troll.py
@@ -1,9 +1,9 @@
 from linkedQfile import *
 from listQfile import ListQ
 
-def main():
-    q = ListQ()
-##    q = LinkedQ()
+def forwardTrick():
+##    q = ListQ()
+    q = LinkedQ()
     k = input("Vilka kort vill du lägga till i listan? Separera siffrorna med ett mellanslag: ")
     k = k.split()
     for i in k:
@@ -16,9 +16,58 @@ def main():
         else:
             y = q.get()
             table.append(y.value)
+    printResult(table)
+##    table_string = ""
+##    for i in table:
+##        table_string += i+" "
+##    print("Resultatet blev:", table_string, "\n")
+
+def backwardTrick():
+    q = ListQ()
+    hand = ListQ()
+    k = input("Vilka kort vill du lägga till i listan? Separera siffrorna med ett mellanslag: ")
+    k = k.split()
+    for i in k:
+        q.put(Node(i))
+    table = []
+    
+    for i in range(len(k)):
+        if hand.isEmpty() == True:
+            x = q.get(-1)
+            hand.put(x)
+        else:
+            x = q.get(-1)
+            hand.put(x, 0)
+            y = hand.get(-1)
+            hand.put(y, 0)
+    printResult(hand.hand)
+##    table_string = ""
+##    for i in hand.hand:
+##        table_string += i.value+" "
+##    print("Resultatet blev:", table_string, "\n")
+
+def printResult(l):
     table_string = ""
-    for i in table:
-        table_string += i+" "
-    print(table_string)
+    for i in l:
+        if isinstance(i, str) == True:
+            table_string += i+" "
+        else:
+            table_string += i.value+" "
+    print("Resultatet blev:", table_string, "\n")
+    
+            
+def main():
+    c = ""
+    print("Hej och välkommen till trolleriprogrammet!")
+    while c != "0":
+        c = input("1 - Vanliga korttricket\n2 - Korttricket baklänges\n0 - Avsluta\n\nGör ditt val: ")
+        if c == "1":
+            forwardTrick()
+        elif c == "2":
+            backwardTrick()
+        elif c == "0":
+            print("Hejdå!")
+        else:
+            print("\nAnge ett giltigt val!")
 
 main()
--- Tilda/listQfile.py
+++ Tilda/listQfile.py
@@ -2,11 +2,14 @@ class ListQ():
 	def __init__(self):
 		self.hand = []	
 
-	def put(self, n):
-		self.hand.append(n)
+	def put(self, n, rev=1):
+                if rev == 1:
+                        self.hand.append(n)
+                else:
+                        self.hand.insert(rev, n)
 
-	def get(self):
-		return self.hand.pop(0)
+	def get(self, i):
+		return self.hand.pop(i)
 
 	def isEmpty(self):
                 if len(self.hand) == 0:
